44191073a6c1eb74ab53efa25526f19c6f13e90b
jeland24 <120576447+jeland24@users.noreply.github.com>
2024-05-21 06:32:50 +0000

    add changes to hello

--- Hello.py
+++ Hello.py
@@ -20,32 +20,18 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Final Project in Cryptography",
+        page_icon="🔐",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-    st.write(" BY: GROUP 14")
+    st.markdown("<h1 style='text-align: center;'>Final Project In<br>Applied Cryptography - CSAC 329</h1>", unsafe_allow_html=True)
+    st.divider()
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    st.markdown("<h3 style='font-weight:bold;'>By: Group 14</h3>", unsafe_allow_html=True)
+    st.write("Espion, Paolo L.")
+    st.write("Albonial, Jeland O.")
+    st.write("Relles, Adrian James.")
+    st.write("BSCS 3B")
 
 
 if __name__ == "__main__":
