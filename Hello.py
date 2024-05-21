7c473d60bf09f7e02e6fb4ee196459689d8a49ea
jeland24 <120576447+jeland24@users.noreply.github.com>
2024-05-21 06:50:11 +0000

    change name

--- Hello.py
+++ Hello.py
@@ -20,7 +20,7 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Final Project in  Applied Cryptography",
+        page_title="Applied Cryptography: Cryptographic Application",
         page_icon="🔐",
     )
 
